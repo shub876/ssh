fddbc54f5984189042e6b145f8fdbfc3635fbdcc
officialputuid <officialputuid@hack.id>
2021-12-16 14:36:09 +0800

    psionic-SSH: Added workspace playground
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Change-Id: Id38abf861d91e63525111b42eda4942dbda53e86

---
+++ workspace.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 Putu Developers
+# Copyright (C) 2021 psionicprjkt
+# Copyright (C) 2021 officialputuid
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+echo psionic-workspace - clone all files needed
+
+# COMING SOON
