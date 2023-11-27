f9a3bff0a7194b756114f1d0bb9ddf6c7bc7d502
officialputuid <officialputuid@hack.id>
2023-11-27 12:11:35 +0800

    psionic-SSH: Update playground
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- workspace.sh
+++ workspace.sh
@@ -8,4 +8,6 @@
 
 echo psionic-workspace - clone all files needed
 
-# COMING SOON
+git clone https://github.com/psionicprjkt/android_kernel_realme_mt6785 mt6785 # Kernel Sources
+git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 mt6785/clang # Clang Sources
+cd mt6785 && rm -rf KernelSU && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - # Setup KernelSU
