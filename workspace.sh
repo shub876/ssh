e531b15c2efa8b1e14523911b4fbd8c6ecb14fbf
Shubham Sharma <131664570+shub876@users.noreply.github.com>
2023-12-24 16:31:07 +0530

    Update workspace.sh

--- workspace.sh
+++ workspace.sh
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021 Putu Developers
-# Copyright (C) 2021 psionicprjkt
-# Copyright (C) 2021 officialputuid
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-echo psionic-workspace - clone all files needed
-
-git clone https://github.com/psionicprjkt/android_kernel_realme_mt6785 mt6785 # Kernel Sources
-git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 mt6785/clang # Clang Sources
-cd mt6785 && rm -rf KernelSU && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - # Setup KernelSU
+sudo apt-get update -y && sudo apt-get install -y neofetch
+neofetch
+df -h
